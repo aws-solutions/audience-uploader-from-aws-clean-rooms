cb50eb8e100f5bb3153393782f9347d58042dcd6
yang qin <yangqqin@amazon.com>
2023-01-31 18:15:38 -0800

    Mock open source dir for pipeline pass

---
+++ deployment/add-open-source-dir.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+deployment_dir="$PWD"
+open_source_dir="$deployment_dir/open-source/"
+
+echo "rm -rf $open_source_dir"
+rm -rf "$open_source_dir"/
+echo "mkdir -p $open_source_dir"
+mkdir -p "$open_source_dir"
